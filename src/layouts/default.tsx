f72bc959e5a6ae641019e0232693d8ce708a5fe9
JAMDev <jamdeveloper0@gmail.com>
2022-08-22 21:44:59 -0300

    Inicialização do projeto concluida

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../components/Header'
 
-export function defaultLayout() {
+export function DefaultLayout() {
   return (
     <>
       <Header />
