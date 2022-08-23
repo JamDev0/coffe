aa791b2818f54a74adf901aca5dbb03abe3afb1a
JAMDev <61752887+JamDev0@users.noreply.github.com>
2022-08-23 10:17:51 -0300

    começo do desenvolvimento do componente apresentação

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,23 @@
+import { HomeContainer, PresentationContainer, PresentationTitles } from "./styles";
+
+import CoffeeImage from '../../assets/CoffeeImage.png'
+
 export function Home() {
-  return <></>
+  return (
+    <HomeContainer>
+      <PresentationContainer>
+        <div>
+          <PresentationTitles>
+            <h1>
+              Encontre o café perfeito para qualquer hora do dia
+            </h1>
+            <h2>
+              Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
+            </h2>
+          </PresentationTitles>
+        </div>
+        <img src={CoffeeImage} />
+      </PresentationContainer>
+    </HomeContainer>
+  )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.body`
+
+`
+
+export const PresentationContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 5.75rem 10rem;
+
+  > div {
+
+  }
+
+  > img {
+    width: 22.5rem;
+  }
+`
+
+export const PresentationTitles = styled.div`
+  > h1 {
+    font-family: 'Balo', serif;
+    font-weight: 800;
+    font-size: 3rem;
+    line-height: 130%;
+  }
+
+  > h2 {
+
+  }
+`
