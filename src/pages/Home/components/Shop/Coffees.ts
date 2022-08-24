9e18978435c5a81875ec9293bbb171519686bf8c
JAMDev <jamdeveloper0@gmail.com>
2022-08-24 10:54:14 -0300

    Implementação de um lista com todos os cafés disponiveis

---
+++ src/pages/Home/components/Shop/Coffees.ts
@@ -0,0 +1,133 @@
+import ExpressoTradicional from '../../../../assets/Coffees/ExpressoTradicional.png'
+
+interface CoffeeInterface {
+  tags?: string[]
+  img: string
+  name: string
+  description: string
+  price: number
+  id: string
+}
+
+export const Coffees: CoffeeInterface[] = [
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+  {
+    name: 'Expresso tradicional',
+    tags: ['Tradicional'],
+    price: 9.99,
+    img: ExpressoTradicional,
+    description: 'Teste',
+    id: String(new Date().getTime()),
+  },
+]
