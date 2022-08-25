8017daefb0785493a5ee108faed1f6181cf8c2c8
JAMDev <jamdeveloper0@gmail.com>
2022-08-25 12:02:33 -0300

    Implementação da página de carrinho no router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { DefaultLayout } from './layouts/default'
+
+import { Cart } from './pages/Cart'
+
 import { Home } from './pages/Home'
 
 export function Router() {
@@ -8,6 +11,7 @@ export function Router() {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
       </Route>
     </Routes>
   )
