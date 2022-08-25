7dafdefb6277aa988792677366961de5997f7088
JAMDev <jamdeveloper0@gmail.com>
2022-08-25 13:43:00 -0300

    Desenvolvimento do Carrinho

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -4,18 +4,36 @@ import { useForm } from 'react-hook-form'
 
 import * as zod from 'zod'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { CartContainer } from './styles'
 
 export function Cart() {
   const OrderFormValidationSchema = zod.object({
     cep: zod.number(),
+    street: zod.string(),
+    streetNumber: zod.number(),
+    streetComplement: zod.optional(zod.string().or(zod.number())),
+    district: zod.string(),
+    city: zod.string(),
+    federativeUnit: zod.string().max(2)
   })
 
-  const OrderForm = useForm()
+  type OrderFormTypes = zod.infer<typeof OrderFormValidationSchema>
+
+  const OrderForm = useForm<OrderFormTypes>(
+    {
+      resolver: zodResolver(OrderFormValidationSchema),
+      defaultValues: {
+      },
+    },
+  )
 
   useEffect(() => {
     document.title = 'Coffee Delivery  - Carrinho'
   }, [])
+
+  // form provider e state do form tbm useFormContext
 
   return <CartContainer></CartContainer>
 }
