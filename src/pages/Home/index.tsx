7b0c5685a297a77922e04bacc11eb8edd7a7fb49
JAMDev <jamdeveloper0@gmail.com>
2022-08-24 23:06:52 -0300

    Implementação de alteração de titulo da página ao carregar o Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,13 @@ import { HomeContainer } from './styles'
 
 import { Presentation } from './components/Presentation'
 import { Shop } from './components/Shop'
+import { useEffect } from 'react'
 
 export function Home() {
+  useEffect(() => {
+    document.title = 'Coffee Delivery - Home'
+  }, [])
+
   return (
     <HomeContainer>
       <Presentation />
