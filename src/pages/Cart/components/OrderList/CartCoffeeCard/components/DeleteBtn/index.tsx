516363c2186c276edf48f9c1f5e9fe2250210c43
JAMDev <jamdeveloper0@gmail.com>
2022-08-29 14:30:55 -0300

    Criação do DeleteBtn

---
+++ src/pages/Cart/components/OrderList/CartCoffeeCard/components/DeleteBtn/index.tsx
@@ -0,0 +1,13 @@
+import { Trash } from "phosphor-react";
+import { DeleteBtnContainer } from "./styles";
+
+export function DeleteBtn() {
+  return (
+    <DeleteBtnContainer>
+      <Trash />
+      <h2>
+        Remover
+      </h2>
+      </DeleteBtnContainer>
+  )
+}
---
+++ src/pages/Cart/components/OrderList/CartCoffeeCard/components/DeleteBtn/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const DeleteBtnContainer = styled.button`
+  display: flex;
+  column-gap: 0.25rem;
+  align-items: center;
+
+  height: 2rem;
+
+  border-radius: 6px;
+  border: none;
+
+  padding: 0.5rem;
+
+  background-color: ${params => params.theme.colors.base.button};
+
+  cursor: pointer;
+
+  > h2 {
+    font-size: 0.75rem;
+    line-height: 160%;
+    text-transform: uppercase;
+    color: ${params => params.theme.colors.base.text};
+  }
+
+  > svg {
+    width: 1rem;
+    height: 1rem;
+
+    color: ${params => params.theme.colors.product.purple[500]};
+  }
+`
