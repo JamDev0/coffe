762a71edd70e69e1ece282e7c09035bdf40ccab5
JAMDev <jamdeveloper0@gmail.com>
2022-09-01 14:27:14 -0300

    Inicio do desenvolvimento do Success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,18 @@
-import { SuccessContainer } from "./styles";
+import { DeliveryInfoContainer, SuccessContainer, SuccessInfoContainer } from "./styles";
+
+import SuccessImg from '../../assets/SuccessImg.jpg'
 
 export function Success() {
-  return <SuccessContainer></SuccessContainer>
+  return (
+    <SuccessContainer>
+      <SuccessInfoContainer>
+        <h1>Uhu! Pedido confirmado</h1>
+        <h2>Agora é só aguardar que logo o café chegará até você</h2>
+        <DeliveryInfoContainer>
+          
+        </DeliveryInfoContainer>
+      </SuccessInfoContainer>
+      <img src={SuccessImg} alt=''/>
+    </SuccessContainer>
+  )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,5 +1,36 @@
 import styled from "styled-components";
 
 export const SuccessContainer = styled.main`
+  display: flex;
+  column-gap: 6.375rem;
 
+  padding: 5rem 10rem;
+
+  > img {
+    width: 30.75rem;
+  }
+`
+
+export const SuccessInfoContainer = styled.section`
+  > h1 {  
+    font-family: 'Baloo 2';
+    font-size: 2rem;
+    font-weight: 800;
+    color: ${params => params.theme.colors.product.yellow[600]};
+  }
+
+  > h2 {
+    font-size: 1.25rem;
+    color: ${params => params.theme.colors.base.subtitle};
+
+    margin-bottom: 2.5rem;
+  }
+`
+
+export const DeliveryInfoContainer = styled.section`
+  display: flex;
+  
+  padding: 2.5rem;
+
+  border-radius: 6px 36px;
 `
