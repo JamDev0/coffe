adef6c052f5b424dab9fbf0cfc4d2974b871cc31
JAMDev <jamdeveloper0@gmail.com>
2022-09-02 16:51:12 -0300

    Implementação do Success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,6 +1,8 @@
-import { DeliveryInfoContainer, SuccessContainer, SuccessInfoContainer } from "./styles";
+import { DeliveryBoldInfo, DeliveryInfo, DeliveryInfosContainer, SuccessContainer, SuccessInfoContainer } from "./styles";
 
-import SuccessImg from '../../assets/SuccessImg.jpg'
+import SuccessImg from '../../assets/SuccessImg.png'
+import { Icon } from "../../components/Icon";
+import { CurrencyCircleDollar, MapPin, Timer } from "phosphor-react";
 
 export function Success() {
   return (
@@ -8,9 +10,34 @@ export function Success() {
       <SuccessInfoContainer>
         <h1>Uhu! Pedido confirmado</h1>
         <h2>Agora é só aguardar que logo o café chegará até você</h2>
-        <DeliveryInfoContainer>
-          
-        </DeliveryInfoContainer>
+        <DeliveryInfosContainer>
+          <DeliveryInfo>
+            <Icon svg={<MapPin weight="fill"/>} backgroundColor='purple' />
+            <p>
+              Entrega em <DeliveryBoldInfo>Rua João Daniel Martinelli, 102</DeliveryBoldInfo> 
+              <br />
+              Farrapos - Porto Alegre, RS
+            </p>
+          </DeliveryInfo>
+
+          <DeliveryInfo>
+            <Icon svg={<Timer weight="fill"/>} backgroundColor='yellow' />
+            <p>
+              Previsão de entrega 
+              <br />
+              <DeliveryBoldInfo>20 min - 30 min</DeliveryBoldInfo>
+            </p>
+          </DeliveryInfo>
+
+          <DeliveryInfo>
+            <Icon svg={<CurrencyCircleDollar weight="regular"/>} backgroundColor='darkYellow' />
+            <p>
+              Pagamento na entrega
+              <br />
+              <DeliveryBoldInfo>Cartão de Crédito</DeliveryBoldInfo>
+            </p>
+          </DeliveryInfo>
+        </DeliveryInfosContainer>
       </SuccessInfoContainer>
       <img src={SuccessImg} alt=''/>
     </SuccessContainer>
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,13 +1,18 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const SuccessContainer = styled.main`
   display: flex;
-  column-gap: 6.375rem;
+  justify-content: space-evenly;
+
+  flex-wrap: wrap;
 
   padding: 5rem 10rem;
 
   > img {
+    align-self: flex-end;
+
     width: 30.75rem;
+    height: fit-content;
   }
 `
 
@@ -27,10 +32,47 @@ export const SuccessInfoContainer = styled.section`
   }
 `
 
-export const DeliveryInfoContainer = styled.section`
+export const DeliveryInfosContainer = styled.section`
+  position: relative;
+
   display: flex;
+  flex-direction: column;
+  row-gap: 2rem;
+
+  width: 32.875rem;
   
   padding: 2.5rem;
 
   border-radius: 6px 36px;
+
+  background-color: ${params => params.theme.colors.base.background};
+
+  ::before {
+    content: '';
+
+    position: absolute;
+    top: 0.0625rem;
+    bottom: 0.0625rem;
+    left: 0.0625rem;
+    right: 0.0625rem;
+    z-index: -1;
+
+    border-radius: 8px 38px;
+
+    background: rgb(219,172,44);
+    background: linear-gradient(115deg, rgba(219,172,44,1) 0%, rgba(128,71,248,1) 100%);
+  }
+`
+
+export const DeliveryInfo = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 0.75rem;
+
+  font-size: 1rem;
+  color: ${params => params.theme.colors.base.text};
+`
+
+export const DeliveryBoldInfo = styled.span`
+  font-weight: 700;
 `
