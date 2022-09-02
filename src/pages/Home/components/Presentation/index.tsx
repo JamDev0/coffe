37f8be39d3f34b1add3f2b48cb0062b2b2635cea
JAMDev <jamdeveloper0@gmail.com>
2022-09-02 16:50:59 -0300

    Implementação do Icon no Presentation

--- src/pages/Home/components/Presentation/index.tsx
+++ src/pages/Home/components/Presentation/index.tsx
@@ -1,11 +1,11 @@
 import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 
 import CoffeeImage from '../../../../assets/CoffeeImage.png'
+import { Icon } from '../../../../components/Icon'
 
 import {
   PresentationContainer,
   PresentationDetailContainer,
-  PresentationDetailIconContainer,
   PresentationDetailsContainer,
   PresentationDetailsSubContainer,
   PresentationTitles,
@@ -26,32 +26,24 @@ export function Presentation() {
         <PresentationDetailsContainer>
           <PresentationDetailsSubContainer>
             <PresentationDetailContainer>
-              <PresentationDetailIconContainer type="cart">
-                <ShoppingCart weight="fill" />
-              </PresentationDetailIconContainer>
+              <Icon svg={<ShoppingCart weight="fill" />} backgroundColor='darkYellow'/>
               <h3>Compra simples e segura</h3>
             </PresentationDetailContainer>
 
             <PresentationDetailContainer>
-              <PresentationDetailIconContainer type="time">
-                <Timer weight="fill" />
-              </PresentationDetailIconContainer>
+              <Icon svg={<Timer weight="fill" />} backgroundColor='yellow'/>
               <h3>Entrega rápida e rastreada</h3>
             </PresentationDetailContainer>
           </PresentationDetailsSubContainer>
 
           <PresentationDetailsSubContainer>
             <PresentationDetailContainer>
-              <PresentationDetailIconContainer type="package">
-                <Package weight="fill" />
-              </PresentationDetailIconContainer>
+              <Icon svg={<Package weight="fill" />} backgroundColor='gray'/>
               <h3>Embalagem mantém o café intacto</h3>
             </PresentationDetailContainer>
 
             <PresentationDetailContainer>
-              <PresentationDetailIconContainer type="coffee">
-                <Coffee weight="fill" />
-              </PresentationDetailIconContainer>
+              <Icon svg={<Coffee weight="fill" />} backgroundColor='purple'/>
               <h3>O café chega fresquinho até você</h3>
             </PresentationDetailContainer>
           </PresentationDetailsSubContainer>
--- src/pages/Home/components/Presentation/styles.ts
+++ src/pages/Home/components/Presentation/styles.ts
@@ -6,21 +6,6 @@ interface PresentationDetailsIconContainerParams {
   type: 'cart' | 'package' | 'time' | 'coffee'
 }
 
-const presentationDetailsIconBackgroundColors = {
-  cart: css`
-    background-color: ${(params) => params.theme.colors.product.yellow[600]};
-  `,
-  package: css`
-    background-color: ${(params) => params.theme.colors.base.title};
-  `,
-  time: css`
-    background-color: ${(params) => params.theme.colors.product.yellow[500]};
-  `,
-  coffee: css`
-    background-color: ${(params) => params.theme.colors.product.purple[500]};
-  `,
-}
-
 export const PresentationContainer = styled.section`
   display: flex;
   justify-content: space-between;
@@ -82,22 +67,5 @@ export const PresentationDetailContainer = styled.div`
     line-height: 130%;
     font-size: 1rem;
     color: ${(params) => params.theme.colors.base.text};
-  }
-`
-
-export const PresentationDetailIconContainer = styled.div<PresentationDetailsIconContainerParams>`
-  padding: 0.5rem;
-
-  border-radius: 9999px;
-
-  line-height: 0;
-
-  ${(params) => presentationDetailsIconBackgroundColors[params.type]};
-
-  > svg {
-    width: 1rem;
-    height: 1rem;
-
-    color: ${(params) => params.theme.colors.base.background};
   }
 `
