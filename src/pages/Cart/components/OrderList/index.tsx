8ccf98f292e3a92df49a806845b77c62a8e8642f
JAMDev <jamdeveloper0@gmail.com>
2022-09-05 23:46:51 -0300

    dinamificação dos CartCoffeeCard e criação de um contexto para evitar propdrilling

--- src/pages/Cart/components/OrderList/index.tsx
+++ src/pages/Cart/components/OrderList/index.tsx
@@ -1,17 +1,33 @@
+import { createContext } from 'react'
+import { useProductCart } from '../../../../hooks/useProductCart'
+import { productInterface } from '../../../../reducers/productCartReducer/reducer'
 import { CartCoffeeCard } from './components/CartCoffeeCard'
 import { ConcludeBtn } from './components/ConcludeBtn'
 import { OrderSummary } from './OrderSummary'
-import { CartCoffeeCardsContainer, OrderContainer, OrderListContainer } from './styles'
+import {
+  CartCoffeeCardsContainer,
+  OrderContainer,
+  OrderListContainer,
+} from './styles'
+
+export const productPropsContext = createContext<productInterface>(
+  {} as productInterface,
+)
 
 export function OrderList() {
+  const { products } = useProductCart()
+
   return (
     <OrderListContainer>
       <h2>Cafés selecionados</h2>
 
       <OrderContainer>
         <CartCoffeeCardsContainer>
-          <CartCoffeeCard />
-          <CartCoffeeCard />
+          {products.map((product) => (
+            <productPropsContext.Provider value={product} key={product.id}>
+              <CartCoffeeCard />
+            </productPropsContext.Provider>
+          ))}
         </CartCoffeeCardsContainer>
         <OrderSummary />
         <ConcludeBtn />
