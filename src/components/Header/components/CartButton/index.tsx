6f682512af4185330f900cd50a4e9a41cca6130d
JAMDev <jamdeveloper0@gmail.com>
2022-09-05 23:38:19 -0300

    implementação da navegação para o carrinho através do CartButton

--- src/components/Header/components/CartButton/index.tsx
+++ src/components/Header/components/CartButton/index.tsx
@@ -1,9 +1,12 @@
 import { ShoppingCart } from 'phosphor-react'
+import { useNavigate } from 'react-router-dom'
 import { ButtonContainer } from './styles'
 
 export function CartButton() {
+  const navigate = useNavigate()
+
   return (
-    <ButtonContainer itemsNumber={null}>
+    <ButtonContainer itemsNumber={null} onClick={() => navigate('/cart')}>
       <ShoppingCart weight="fill" />
     </ButtonContainer>
   )
