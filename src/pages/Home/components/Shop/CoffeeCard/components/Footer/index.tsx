197b7e11595facbf16c490aac355a7c57475b305
JAMDev <jamdeveloper0@gmail.com>
2022-09-05 23:53:58 -0300

    Implementação da função de navegação para o cart apartir do Footer do CoffeeCard da home e envio das informações para o QuantityCounter

--- src/pages/Home/components/Shop/CoffeeCard/components/Footer/index.tsx
+++ src/pages/Home/components/Shop/CoffeeCard/components/Footer/index.tsx
@@ -1,23 +1,26 @@
 import { ShoppingCart } from 'phosphor-react'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { CoffeeCardPropsContext } from '../../..'
 import { QuantityCounter } from '../../../../../../../components/QuantityCounter'
-import {
-  FooterContainer,
-  Price,
-  ShopCartBtn,
-} from './styles'
+import { FooterContainer, Price, ShopCartBtn } from './styles'
 
 interface FooterProps {
   price: number
 }
 
 export function Footer({ price }: FooterProps) {
+  const coffeeInfo = useContext(CoffeeCardPropsContext)
+
+  const navigate = useNavigate()
+
   return (
     <FooterContainer>
       <span>
         R$ <Price>{price}</Price>
       </span>
-      <QuantityCounter />
-      <ShopCartBtn>
+      <QuantityCounter coffeeInfo={coffeeInfo} />
+      <ShopCartBtn onClick={() => navigate('/cart')}>
         <ShoppingCart weight="fill" />
       </ShopCartBtn>
     </FooterContainer>
