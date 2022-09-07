404f53f3646334f0e80ebce7ad0932ead5bda4bc
JAMDev <jamdeveloper0@gmail.com>
2022-09-07 14:24:16 -0300

    Implementação da função de enviar o formulário de compra no botão ConcludeBtn

--- src/pages/Cart/components/OrderList/components/ConcludeBtn/index.tsx
+++ src/pages/Cart/components/OrderList/components/ConcludeBtn/index.tsx
@@ -1,5 +1,30 @@
-import { ConcludeBtnContainer } from "./styles";
+import { useFormContext } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { usePaymentForm } from '../../../../../../hooks/usePaymentForm'
+import { ConcludeBtnContainer } from './styles'
 
 export function ConcludeBtn() {
-  return <ConcludeBtnContainer>confirmar pedido</ConcludeBtnContainer>
+  const { handleSubmit } = useFormContext()
+
+  const { selectedPaymentForm } = usePaymentForm()
+
+  const navigate = useNavigate()
+
+  function onSubmit(data) {
+    const isPaymentFormSelected = selectedPaymentForm !== null
+
+    if (isPaymentFormSelected) {
+      navigate('/success')
+    }
+  }
+
+  function onError(data) {
+    console.log('Erro ', data)
+  }
+
+  return (
+    <ConcludeBtnContainer onClick={() => handleSubmit(onSubmit, onError)()}>
+      confirmar pedido
+    </ConcludeBtnContainer>
+  )
 }
