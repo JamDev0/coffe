2f2546b3168c91240f486a4a0a4f62fcd472e8b9
JAMDev <jamdeveloper0@gmail.com>
2022-09-08 23:27:15 -0300

    Começo da dinamifacação do Success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,10 +1,32 @@
-import { DeliveryBoldInfo, DeliveryInfo, DeliveryInfosContainer, SuccessContainer, SuccessInfoContainer } from "./styles";
+import {
+  DeliveryBoldInfo,
+  DeliveryInfo,
+  DeliveryInfosContainer,
+  SuccessContainer,
+  SuccessInfoContainer,
+} from './styles'
 
 import SuccessImg from '../../assets/SuccessImg.png'
-import { Icon } from "../../components/Icon";
-import { CurrencyCircleDollar, MapPin, Timer } from "phosphor-react";
+import { Icon } from '../../components/Icon'
+import { CurrencyCircleDollar, MapPin, Timer } from 'phosphor-react'
+import { usePaymentForm } from '../../hooks/usePaymentForm'
 
 export function Success() {
+  const { selectedPaymentForm } = usePaymentForm()
+
+  function selectedPaymentFormTextToDisplay() {
+    switch (selectedPaymentForm) {
+      case 'cash':
+        return 'Dinheiro'
+
+      case 'credit':
+        return 'Cartão de Crédito'
+
+      case 'debit':
+        return 'Cartão de Débito'
+    }
+  }
+
   return (
     <SuccessContainer>
       <SuccessInfoContainer>
@@ -12,34 +34,42 @@ export function Success() {
         <h2>Agora é só aguardar que logo o café chegará até você</h2>
         <DeliveryInfosContainer>
           <DeliveryInfo>
-            <Icon svg={<MapPin weight="fill"/>} backgroundColor='purple' />
+            <Icon svg={<MapPin weight="fill" />} backgroundColor="purple" />
             <p>
-              Entrega em <DeliveryBoldInfo>Rua João Daniel Martinelli, 102</DeliveryBoldInfo> 
+              Entrega em{' '}
+              <DeliveryBoldInfo>
+                Rua João Daniel Martinelli, 102
+              </DeliveryBoldInfo>
               <br />
               Farrapos - Porto Alegre, RS
             </p>
           </DeliveryInfo>
 
           <DeliveryInfo>
-            <Icon svg={<Timer weight="fill"/>} backgroundColor='yellow' />
+            <Icon svg={<Timer weight="fill" />} backgroundColor="yellow" />
             <p>
-              Previsão de entrega 
+              Previsão de entrega
               <br />
               <DeliveryBoldInfo>20 min - 30 min</DeliveryBoldInfo>
             </p>
           </DeliveryInfo>
 
           <DeliveryInfo>
-            <Icon svg={<CurrencyCircleDollar weight="regular"/>} backgroundColor='darkYellow' />
+            <Icon
+              svg={<CurrencyCircleDollar weight="regular" />}
+              backgroundColor="darkYellow"
+            />
             <p>
               Pagamento na entrega
               <br />
-              <DeliveryBoldInfo>Cartão de Crédito</DeliveryBoldInfo>
+              <DeliveryBoldInfo>
+                {selectedPaymentFormTextToDisplay()}
+              </DeliveryBoldInfo>
             </p>
           </DeliveryInfo>
         </DeliveryInfosContainer>
       </SuccessInfoContainer>
-      <img src={SuccessImg} alt=''/>
+      <img src={SuccessImg} alt="" />
     </SuccessContainer>
   )
 }
