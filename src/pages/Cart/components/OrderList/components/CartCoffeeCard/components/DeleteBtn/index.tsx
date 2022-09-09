00fcdc34c526650bddb6512337adac32ac37107c
JAMDev <jamdeveloper0@gmail.com>
2022-09-08 23:14:54 -0300

    Implementação da função removeProduct ao clicar no DeleteBtn

--- src/pages/Cart/components/OrderList/components/CartCoffeeCard/components/DeleteBtn/index.tsx
+++ src/pages/Cart/components/OrderList/components/CartCoffeeCard/components/DeleteBtn/index.tsx
@@ -1,9 +1,16 @@
 import { Trash } from 'phosphor-react'
+import { useContext } from 'react'
+import { productPropsContext } from '../../../..'
+import { useProductCart } from '../../../../../../../../hooks/useProductCart'
 import { DeleteBtnContainer } from './styles'
 
 export function DeleteBtn() {
+  const { removeProduct } = useProductCart()
+
+  const { id } = useContext(productPropsContext)
+
   return (
-    <DeleteBtnContainer>
+    <DeleteBtnContainer onClick={() => removeProduct(id)}>
       <Trash />
       <h2>Remover</h2>
     </DeleteBtnContainer>
